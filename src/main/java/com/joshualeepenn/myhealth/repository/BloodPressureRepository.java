c7aaecc2324c95ab60e6093dc0829b0fa51c0908
Joshua Penn <joshualeepenn@gmail.com>
2024-10-06 15:47:48 -0400

    #1 Use CrudRepository

--- src/main/java/com/joshualeepenn/myhealth/repository/BloodPressureRepository.java
+++ src/main/java/com/joshualeepenn/myhealth/repository/BloodPressureRepository.java
@@ -1,8 +1,8 @@
 package com.joshualeepenn.myhealth.repository;
 
 import com.joshualeepenn.myhealth.model.BloodPressure;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 
-public interface BloodPressureRepository extends Repository<BloodPressure, Integer> {
+public interface BloodPressureRepository extends CrudRepository<BloodPressure, Integer> {
 }
--- src/main/java/com/joshualeepenn/myhealth/repository/UserRepository.java
+++ src/main/java/com/joshualeepenn/myhealth/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.joshualeepenn.myhealth.repository;
 
 import com.joshualeepenn.myhealth.model.User;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends Repository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
